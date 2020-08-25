35746c0deda1eec305b18697934a20cec8657ec7
DESKTOP-MIT9Q8B\Yaskevich <andrez.yaskevich@gmail.com>
2020-08-26 01:11:49 +0300

    Create Album and Song entities

--- CodeFirstAlbum/Song.cs
+++ CodeFirstAlbum/Song.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
-
+        
         public int? AlbumId { get; set; }
         public Album Album { get; set; }
     }
